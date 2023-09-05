18cfa858c7af6b7098a05da5b6bab22ee8bd866a
Seif Albaghdady <85033207+seifAlbaghdady@users.noreply.github.com>
2023-09-05 04:41:40 +0300

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.get('/', (req, res) => {
 });
 
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}!`);
 });
